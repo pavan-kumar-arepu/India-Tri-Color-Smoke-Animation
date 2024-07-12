3e2cb0cb7800c5d9bd498b5b7661efa605b6050f
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-07-12 21:34:47 +0200

    Initial Commit

---
+++ SmokeTest/SmokeTestApp.swift
@@ -0,0 +1,18 @@
+//
+//  SmokeTestApp.swift
+//  SmokeTest
+//
+//  Created by Pavankumar Arepu on 12/07/24.
+//
+
+import SwiftUI
+import IndianFlagSmokeEffect
+
+@main
+struct SmokeTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
