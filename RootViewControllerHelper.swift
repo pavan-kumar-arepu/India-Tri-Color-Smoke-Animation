62669f1855f2c86fb26e7cee6a5d2b6c418aa0f5
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-07-12 21:58:29 +0200

    Update the package  and added a button to show the IndiaColor Smoke

---
+++ RootViewControllerHelper.swift
@@ -0,0 +1,35 @@
+//
+//  RootViewControllerHelper.swift
+//  SmokeTest
+//
+//  Created by Pavankumar Arepu on 12/07/24.
+//
+
+import Foundation
+import SwiftUI
+import UIKit
+
+struct RootViewControllerKey: EnvironmentKey {
+    static var defaultValue: UIViewController? {
+        return UIApplication.shared.windows.first?.rootViewController
+    }
+}
+
+extension EnvironmentValues {
+    var rootViewController: UIViewController? {
+        get { self[RootViewControllerKey.self] }
+        set { self[RootViewControllerKey.self] = newValue }
+    }
+}
+
+struct RootViewControllerAccessor: UIViewControllerRepresentable {
+    func makeUIViewController(context: Context) -> UIViewController {
+        let viewController = UIViewController()
+        DispatchQueue.main.async {
+            viewController.viewDidAppear(false)
+        }
+        return viewController
+    }
+    
+    func updateUIViewController(_ uiViewController: UIViewController, context: Context) {}
+}
--- SmokeTest/ContentView.swift
+++ SmokeTest/ContentView.swift
@@ -6,18 +6,38 @@
 //
 
 import SwiftUI
+import IndianFlagSmokeEffect
+
 
 struct ContentView: View {
+    @Environment(\.rootViewController) private var rootViewController
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                Text("Your App Content")
+                    .foregroundColor(.white)
+                    .padding()
+                
+                Button(action: {
+                    if let rootViewController = rootViewController {
+                        SmokeEffectPresenter.presentSmokeEffect(from: rootViewController)
+                    }
+                }) {
+                    Text("Show Smoke Effect")
+                        .foregroundColor(.white)
+                        .padding()
+                        .background(Color.blue)
+                        .cornerRadius(8)
+                }
+            }
         }
-        .padding()
+        .background(RootViewControllerAccessor().hidden())
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
