74a2ebac9ab2992a98ad4cc669ff9be3dc1c3762
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-07-13 22:08:25 +0200

    Updated ContentView

--- SmokeTest/ContentView.swift
+++ SmokeTest/ContentView.swift
@@ -4,25 +4,44 @@
 //
 //  Created by Pavankumar Arepu on 12/07/24.
 //
+
 import SwiftUI
 import IndianFlagSmokeEffect
+
 
 struct ContentView: View {
     @Environment(\.rootViewController) private var rootViewController
     
     var body: some View {
         ZStack {
-    
-            Color.black.edgesIgnoringSafeArea(.all)
-            
+            Color.white.edgesIgnoringSafeArea(.all)
+
             VStack {
                 Spacer()
-                    .foregroundColor(.white)
-                    .padding()
+
                 
+                Text("Indian-Tri-Color Animation \nFrom \n Swift Package Manager")
+                                 .font(.title)
+                                 .fontWeight(.bold)
+                                 .frame(maxWidth: .infinity)
+                                 .multilineTextAlignment(.center)
+                                 .padding()
+                Spacer()
+
+                
+                Text("GitHub: https://github.com/pavan-kumar-arepu/IndianFlagSmokeEffect-SwiftPackage.git")
+                                 .font(.title)
+                                 .fontWeight(.thin)
+                                 .frame(maxWidth: .infinity)
+                                 .multilineTextAlignment(.center)
+                                 .padding()
+                
+            
+                Spacer()
+
                 Button(action: {
                     if let rootViewController = rootViewController {
-                        SmokeEffectPresenter.presentSmokeEffect(from: rootViewController)
+                        IndianFlagSmokeEffect.presentSmokeEffect(from: rootViewController)
                     }
                 }) {
                     Text("Show Smoke Effect")
@@ -34,10 +53,9 @@ struct ContentView: View {
                 }
             }
         }
-        .background(RootViewControllerAccessorNew().hidden())
+        .background(PackageRootViewControllerAccessor().hidden())
     }
 }
-
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
